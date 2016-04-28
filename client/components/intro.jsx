1ffe38ce29957a18e060af86c46a47d80b9cdd0d
alexjhannan <alexjhannan@gmail.com>
2016-04-28 17:28:45 -0400

    Added intro text

---
+++ client/components/intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default Intro = () => (
+	<div className="intro--textBox">
+		<p>Hi! My name is Alex, and <span className="accentText">I’m a self-taught developer</span> in NYC.</p> 
+		<p>A year ago, I was teaching <span className="accentText">calculus and physics</span> at a private high school. Disappointing career options had my eyes wandering towards other paths. The market is pushing towards tech, like it always has, and I have always been a techy at heart. It just made sense to make the switch.</p>
+		<p>I was a bit skeptical of the canned education that bootcamps were offering, and I was also pretty confident in managing an educational plan. <span className="accentText">So, I taught myself how to code.</span> It didn’t hurt that I studied physics in school and had already learned many of the underlying concepts. <span className="accentText">Before I knew it, I was building and deploying web pages by myself.</span> Nowadays, I play with design on top of the full stack, and am always looking for the next skill to add to my capabilities.</p>
+		<p>If you’re a part of a team that’s interested in an <span className="accentText">ambitious and independent coder</span>, or a client interested in hiring for some contract work, please <span className="accentText">feel free to email me</span> at alexjhannan@gmail.com! I am currently looking for both full-time positions and contracts.</p>
+		<p>Thanks, and have a great day!</p>
+		<p>PS: Check out the table below for a brief overview of my skill set, my resume for a more in-depth look, or just “pick my brain” for some interesting links to get a sense of who I am.</p>
+	</div>
+)
--- client/views/mainView.jsx
+++ client/views/mainView.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PickMyBrain from '/client/containers/pickMyBrain.jsx';
+import Intro from '/client/components/intro.jsx';
 
 let MainView = React.createClass({
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col s12 m6">
+					<div className="col s12 m6 l8">
 						<img src={"/assets/intro--logo.png"} />
+						<Intro />
 					</div>
-					<div className="col s12 m6">
+					<div className="col s12 m6 l4">
 						<div className="mainView--spacer hide-on-small-only"></div>
 						<PickMyBrain />
 					</div>

47b5071a377c0a66eff744e8c299779e8d68c22a
alexjhannan <alexjhannan@gmail.com>
2016-04-30 13:17:34 -0400

    Added vertical alignment for large displays

--- client/views/mainView.jsx
+++ client/views/mainView.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 let MainView = React.createClass({
+	componentDidMount() {
+		$(window).resize(this.addVerticalOffset);
+	},
+	addVerticalOffset() {
+		let windowHeight = $(window).height();
+		let contentHeight = $(".mainView--container").height();
+
+		if (windowHeight > contentHeight) {
+			let verticalOffset = (windowHeight - contentHeight) / 2.2 + "px";
+			$(".mainView--container").css("marginTop", verticalOffset);
+		} else {
+			$(".mainView--container").css("marginTop", 0);
+		}
+	},
 	render() {
 		return (
-			<div className="container">
+			<div className="container mainView--container">
 				<div className="row">
 					<div className="col s12 l10 offset-l1">
 						<Intro />
